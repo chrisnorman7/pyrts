2ed4a109a100adc8c4c4749e49de3f91f4d9f133
Chris Norman <chris.norman2@googlemail.com>
2017-03-23 23:00:18 +0000

    Added player-side connect / disconnect notifications.

--- server.py
+++ server.py
@@ -15,25 +15,32 @@ from commands.base import commands
 class Protocol(LineReceiver):
     """The protocol for dealing with clients."""
     def connectionMade(self):
-        self.game = session.query(Game).first()
-        if self.game is None:
-            self.game = Game()
-            self.game.save()
-            log.msg('Created new game %r.' % self.game)
+        game = session.query(Game).first()
+        if game is None:
+            game = Game()
+            game.save()
+            log.msg('Created new game %r.' % game)
         self.player = Player(
-            game=self.game,
+            game=game,
             name='Player',
-            start_x=float(floor(random() * self.game.size_x)),
-            start_y=float(floor(random() * self.game.size_y)),
+            start_x=float(floor(random() * game.size_x)),
+            start_y=float(floor(random() * game.size_y)),
         )
         self.player.connection = self
         self.player.save()
         self.player.name = 'Player %d' % self.player.id
+        for obj in session.query(
+            Player
+        ).filter(
+            Player.game == game,
+            Player.id != self.player.id
+        ):
+            obj.notify('{} connected.', self.player.name)
         self.player.save()
         log.msg('Created player %r.' % self.player)
         # Give the player their first building:
         building = GameObject(
-            game=self.game,
+            game=game,
             owner=self.player,
             x=self.player.start_x,
             y=self.player.start_y
@@ -44,8 +51,8 @@ class Protocol(LineReceiver):
         building.type = town_hall
         self.player.notify(
             'The board is {} by {}.',
-            self.game.size_x,
-            self.game.size_y
+            game.size_x,
+            game.size_y
         )
         self.player.notify(
             'Your location: ({}, {}).',
@@ -62,13 +69,21 @@ class Protocol(LineReceiver):
                 reason.getErrorMessage()
             )
         )
+        game = self.player.game
+        for obj in session.query(
+            Player
+        ).filter(
+            Player.game == self.player.game,
+            Player.id != self.player.id
+        ):
+            obj.notify('{} disconnected.', self.player.name)
         for obj in [self.player, *self.player.owned_objects]:
             log.msg('Deleting object %r.' % obj)
             session.delete(obj)
         session.commit()
-        if not self.game.objects and not self.game.players:
-            log.msg('Deleting game %r.' % self.game)
-            session.delete(self.game)
+        if not game.objects and not game.players:
+            log.msg('Deleting game %r.' % game)
+            session.delete(game)
             session.commit()
 
     def lineReceived(self, string):
