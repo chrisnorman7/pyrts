ae3ab55dcb5adcec15ada2c5564fbcc39edeff75
Chris Norman <chris.norman2@googlemail.com>
2019-09-27 22:46:21 +0100

    Check for launched transports when starting the server.

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from autobahn.twisted.websocket import listenWS, WebSocketServerFactory
 from twisted.internet import reactor
 from twisted.web.server import Site
 
-from server.db import Base, load, dump, setup, Unit
+from server.db import Base, load, dump, setup, Unit, Transport
 from server.options import options
 from server.tasks import task
 from server.util import pluralise
@@ -46,6 +46,10 @@ def main():
                 m.start_task()
         else:
             logging.info('No tasks to resume.')
+        logging.info('Phase: Check for airborn transports.')
+        for t in Transport.all(Transport.land_time > time()):
+            logging.info('Landing %s.', t.unit.get_name())
+            Transport.land(t.id)
     except FileNotFoundError:
         logging.info('Starting with a blank database.')
     logging.info('Phase: Database setup.')
