bdb38a935dbbe6b70b0b960ba132dc6870455ceb
Chris Norman <chris.norman2@googlemail.com>
2019-10-02 14:30:22 +0100

    Check that start building can recruit something.

--- validate.py
+++ validate.py
@@ -1,12 +1,14 @@
 """Check for inconsistancies in the database."""
 
 from server.db import (
-    BuildingBuilder, BuildingRecruit, BuildingType, load, UnitType
+    BuildingBuilder, BuildingRecruit, BuildingType, load, UnitType, setup,
+    options
 )
 
 
 def main():
     load()
+    setup()
     for name in UnitType.resource_names():
         if UnitType.count(getattr(UnitType, name) >= 1):
             continue
@@ -18,6 +20,9 @@ def main():
     for ut in UnitType.all():
         if not BuildingRecruit.count(unit_type_id=ut.id):
             print(f'There is no way to recruit {ut.get_name()}.')
+    sb = options.start_building
+    if not sb.recruits:
+        print(f'Start building {sb.get_name()} cannot recruit anything.')
 
 
 if __name__ == '__main__':
