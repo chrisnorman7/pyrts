e3828ae72ae778461eee9b93ccc87167505f91f8
Chris Norman <chris.norman2@googlemail.com>
2019-10-02 14:28:25 +0100

    Make sure all unit types can be recruited.

--- validate.py
+++ validate.py
@@ -1,18 +1,23 @@
 """Check for inconsistancies in the database."""
 
-from server.db import BuildingBuilder, BuildingType, load, UnitType
+from server.db import (
+    BuildingBuilder, BuildingRecruit, BuildingType, load, UnitType
+)
 
 
 def main():
     load()
     for name in UnitType.resource_names():
-        if UnitType.count(getattr(UnitType, name) == 1):
+        if UnitType.count(getattr(UnitType, name) >= 1):
             continue
         else:
             print(f'There is no unit that can gather {name}.')
     for bt in BuildingType.all():
         if not BuildingBuilder.count(building_type_id=bt.id):
             print(f'There is no way to build {bt.name}.')
+    for ut in UnitType.all():
+        if not BuildingRecruit.count(unit_type_id=ut.id):
+            print(f'There is no way to recruit {ut.get_name()}.')
 
 
 if __name__ == '__main__':
